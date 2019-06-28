9abb602c6501d1c25051a1d9e5fda95fd4d5c7d3
Arthur Zhang <arthurzwl@gmail.com>
2019-06-28 21:41:15 +0800

    remove comment

--- java/113.path-sum-ii.java
+++ java/113.path-sum-ii.java
@@ -2,43 +2,6 @@
  * @lc app=leetcode id=113 lang=java
  *
  * [113] Path Sum II
- *
- * https://leetcode.com/problems/path-sum-ii/description/
- *
- * algorithms
- * Medium (39.29%)
- * Total Accepted:    224.1K
- * Total Submissions: 558K
- * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
- *
- * Given a binary tree and a sum, find all root-to-leaf paths where each path's
- * sum equals the given sum.
- * 
- * Note: A leaf is a node with no children.
- * 
- * Example:
- * 
- * Given the below binary tree and sum = 22,
- * 
- * 
- * ⁠     5
- * ⁠    / \
- * ⁠   4   8
- * ⁠  /   / \
- * ⁠ 11  13  4
- * ⁠/  \    / \
- * 7    2  5   1
- * 
- * 
- * Return:
- * 
- * 
- * [
- * ⁠  [5,4,11,2],
- * ⁠  [5,8,4,5]
- * ]
- * 
- * 
  */
 /**
  * Definition for a binary tree node.
---
+++ java/39.combination-sum.java
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=39 lang=java
+ *
+ * [39] Combination Sum
+ */
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+    }
+}
+
